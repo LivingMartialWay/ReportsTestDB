01fdc4d9459437c99ffbc87ae2d31f15a47c4307
livingmartialway <livingmartialway@gmail.com>
2023-03-20 12:26:34 -0500

    Dogshitlord

---
+++ ReportTestWeb/Pages/DropLogSearch.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ReportTestWeb.Pages
+{
+    public class DropLogSearchModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ReportTestWeb/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ReportTestWeb.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
