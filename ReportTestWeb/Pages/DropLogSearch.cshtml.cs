bde9f88c6b6f19697fd5c57e3fccaae0abbc5ed9
livingmartialway <livingmartialway@gmail.com>
2023-03-20 13:39:50 -0500

    FUCK YOU!

--- ReportTestWeb/Pages/DropLogSearch.cshtml.cs
+++ ReportTestWeb/Pages/DropLogSearch.cshtml.cs
@@ -5,6 +5,9 @@ namespace ReportTestWeb.Pages
 {
     public class DropLogSearchModel : PageModel
     {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
         public void OnGet()
         {
         }
